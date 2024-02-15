6af5cf55a557af0ec40e45f15697793ded82999a
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-02-14 21:12:03 -0300

    segundo commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,47 +1,60 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function Home() {
-  const [slideIndex, setSlideIndex] = useState(0);
   const [updateCount, setUpdateCount] = useState(0);
-  let sliderRef = useRef(null);
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     afterChange: () => setUpdateCount(updateCount + 1),
-    beforeChange: (current, next) => setSlideIndex(next),
+  };
+
+  const imageStyle = {
+    width: "90%",
+    height: "50%",
+    borderRadius: "8px",
+  };
+
+  const containerStyle = {
+    display: "block",
   };
 
   return (
-    <div className="slider-container">
+    <div className="slider-container" style={containerStyle}>
       <p>Total updates: {updateCount} </p>
-      <input
-        onChange={(e) => sliderRef.slickGoTo(e.target.value)}
-        value={slideIndex}
-        type="range"
-      />
-      <Slider
-        ref={(slider) => {
-          sliderRef = slider;
-        }}
-        {...settings}
-        >
+      <Slider {...settings}>
         <div>
-          <img src={process.env.PUBLIC_URL + "imagens/pexels-aleksandar-pasaric-618079.jpg"} alt="Imagem 1" />
+          <img
+            src="/imagens/pexels-aleksandar-pasaric-618079.jpg"
+            alt="Imagem 1"
+            style={imageStyle}
+          />
         </div>
         <div>
-          <img src={process.env.PUBLIC_URL + "/pexels-chris-schippers-421927.jpg"} alt="Imagem 2" />
+          <img
+            src="/imagens/pexels-chris-schippers-421927.jpg"
+            alt="Imagem 2"
+            style={imageStyle}
+          />
         </div>
         <div>
-          <img src={process.env.PUBLIC_URL + "/pexels-francesco-ungaro-409127.jpg"} alt="Imagem 3" />
+          <img
+            src="/imagens/pexels-francesco-ungaro-409127.jpg"
+            alt="Imagem 3"
+            style={imageStyle}
+          />
         </div>
         <div>
-          <img src={process.env.PUBLIC_URL + "/pexels-lukas-kloeppel-466685.jpg"} alt="Imagem 4" />
+          <img
+            src="/imagens/pexels-lukas-kloeppel-466685.jpg"
+            alt="Imagem 4"
+            style={imageStyle}
+          />
         </div>
       </Slider>
     </div>
