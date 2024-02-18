43ec6a96b33934c0c20a1f25c394ba800e5bd0c1
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-02-18 17:24:47 -0300

    Code Together

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -40,8 +40,6 @@ const CustomMenu = () => {
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
-
-              {/* Profile dropdown */}
               <Menu as="div" className="relative ml-3">
                 <Transition
                   as={Fragment}
